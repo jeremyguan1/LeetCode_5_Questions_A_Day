692468921cfb2b742179cfc3ec4cbaa85b350feb
jeremyguan1 <jeremyguan1@gmail.com>
2020-08-05 23:21:20 -0700

    Update 7_Reverse_Integer.js

--- 7_Reverse_Integer.js
+++ 7_Reverse_Integer.js
@@ -3,15 +3,11 @@
  * @return {number}
  */
 var reverse = function(x) {
-  let negative = x < 0 ? true : false;
-  if(x < 10 && x >= 0) {
-      return x;
-  }
   let number = Number.parseInt(Math.abs(x).toString().split('').reverse().join(''));
   if(Math.pow(2,31)-1 < number ||Math.pow(-2,31) > number){
       return 0
   }
-  return negative ? -1 * Number.parseInt(Math.abs(x).toString().split('').reverse().join('')) : Number.parseInt(Math.abs(x).toString().split('').reverse().join(''))
+  return Math.sign(x) * Number.parseInt(Math.abs(x).toString().split('').reverse().join(''))
 };
 
 console.log(reverse(-230))
